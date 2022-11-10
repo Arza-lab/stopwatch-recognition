5fcaf578748f3905e8746a2b29abd3c663981d7b
Maurice Schwarz <schwarz@desginpark.de>
2022-11-10 14:30:33 +0100

    feat: Logik zum handhaben von File Requests

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\StopwatchRecord;
+use App\Repository\StopwatchRecordRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\FileBag;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use thiagoalessio\TesseractOCR\TesseractOCR;
@@ -10,17 +14,55 @@ use thiagoalessio\TesseractOCR\TesseractOcrException;
 
 class ImageController extends AbstractController
 {
-    /**
-     * @throws TesseractOcrException
-     */
-    #[Route('/api/_action/image', name: 'app_image_index')]
-    public function index(): Response
+    #[Route('/api/_action/image', name: 'app_image_index', methods: ['POST'])]
+    public function index(FileBag $fileBag, ParameterBag $parameterBag, StopwatchRecordRepository $stopwatchRecordRepository): Response
     {
-        // $image = 'stoppuhr.jpeg';
-        $image = 'digitale-stopp-uhr-stoppuhr_2.jpeg';
+        $file = $fileBag->get('file');
+        $lang = $parameterBag->get('lang', 'eng');
 
-        $interpreter = (new TesseractOCR($image))->psm(11);
+        $ocr = $this->getInterpreterWithImage($file->getPathname());
 
-        dd($interpreter->run());
+        try {
+            $text = $ocr->run();
+            if ($text) {
+                $record = new StopwatchRecord();
+                $record->setTime($text);
+                $stopwatchRecordRepository->save($record, true);
+
+                return $this->json([
+                    'success' => true,
+                    'text' => $text,
+                ]);
+            }
+        } catch (TesseractOcrException $e) {
+            return $this->json([
+                'success' => false,
+                'error' => $e->getMessage(),
+            ]);
+        }
+
+        return $this->json([
+            'success' => false,
+        ]);
+    }
+
+    private function getInterpreter(): TesseractOCR
+    {
+        return (new TesseractOCR())->psm(11);
+    }
+
+    private function getInterpreterWithImage(string $image): TesseractOCR
+    {
+        return $this->getInterpreter()->image($image);
+    }
+
+    #[Route('/api/_action/image/test', name: 'app_image_test')]
+    public function test(): void
+    {
+        $interpreter = $this->getInterpreterWithImage('stoppuhr.jpeg');
+        try {
+            $interpreter->run();
+        } catch (TesseractOcrException $e) {
+        }
     }
 }
