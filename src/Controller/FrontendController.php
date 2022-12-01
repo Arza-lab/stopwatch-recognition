9b7baba62df09e14e3e70ec947ca7e45f60e38d5
Richard Senst <senst_richard@oszimt.de>
2022-12-01 14:23:05 +0100

    image crop by color

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -11,74 +11,106 @@ class FrontendController extends AbstractController
     #[Route('/', name: 'app_frontend')]
     public function index(): Response
     {
-
-        $imgWidth = 100;
-        $imgHeight = 100;
-        $gradientImg = imagecreatetruecolor($imgWidth,$imgHeight);
-
-        $this -> image_gradientrect($gradientImg,0,0,$imgWidth,$imgHeight,'94a69d','0c3721');
-        imagepng($gradientImg,'gradient.png');
-
-
-        $image = imageCreateFromPng('Unbenannt.png');
+        
+        $image = imageCreateFromJpeg('digitale-stopp-uhr-stoppuhr.jpeg');
         $width = imagesx($image);
         $height = imagesy($image);
 
         $arr = array();
-        $index = 0;
         $text = "";
-        
+
+        $rmax = 148;
+        $rmin = 12;
+        $gmax = 166;
+        $gmin = 55;
+        $bmax = 157;
+        $bmin = 33;
+
         for($x = 0; $x < $width; $x++) {
             for($y = 0; $y < $height; $y++) {
-                // pixel color at (x, y)
                 $color = imagecolorat($image, $x, $y);
                 $col = imagecolorsforindex($image,$color);
-                $r = ($color >> 16) & 0xFF;
-                $g = ($color >> 8) & 0xFF;
-                $b = $color & 0xFF;
-                array_push($arr, "$r, $g, $b");
-                // print_r($col);
-                $text .= "$x / $y : $col[red], $col[green], $col[blue], $col[alpha] </br>";
-                $index = $index + 1;
+                $r = $col["red"];
+                $g = $col["green"];
+                $b = $col["blue"];
+
+
+                if ((($rmin <= $r) && ($r <= $rmax)) && (($gmin <= $g) && ($g <= $gmax)) && (($bmin <= $b) && ($b <= $bmax)) && (($r < $g) && ($b <= $g))){
+                    array_push($arr, "$x/$y");
+                }
+                else {
+                    $red = imagecolorallocate($image, 0, 0, 0); 
+                }
             }
         }
+    
+        $checkedArr = array_filter($arr, function($value) use ($arr) {
+            $val = explode("/", $value,2);
+            $x = $val[0];
+            $y = $val[1];
+            $valuesToCheck = array(($x + 1)."/".($y),($x + 2)."/".($y),($x)."/".($y+1),($x)."/".($y+2),($x + 1)."/".($y+1),($x + 2)."/".($y+2),
+                             ($x - 1)."/".($y),($x - 2)."/".($y),($x)."/".($y-1),($x)."/".($y-2),($x - 1)."/".($y-1),($x - 2)."/".($y-2));
+
+
+            $checks = [];
+            foreach ($valuesToCheck as $checkvalue) {
+                if (in_array($checkvalue, $arr)){
+                    array_push($checks, $checkvalue);
+                }            
+                if (count($checks) >= 3) {
+                    break;
+                }
+            }
+
+            return count($checks) >= 3;    
+        });
+
+        $minx = $width;
+        $maxx = 0;
+
+        $miny = $height;
+        $maxy = 0;
+
+        foreach ($checkedArr as $value) {
+            $val = explode("/", $value,2);
+            //dd($val);
+            $x = $val[0];
+            $y = $val[1];
+
+            //dd($arr,$x,$y);
+            if ($x < $minx){
+                $minx = $x;
+            }
+            if ($x > $maxx){
+                $maxx = $x;
+            }
+            if ($y < $miny){
+                $miny = $y;
+            }
+            if ($y > $maxy){
+                $maxy = $y;
+            }
+        }
+
+        $im2 = imageCrop($image, ['x' => $minx, 'y' => $miny, 'width' => ($maxx - $minx) + 5, 'height' =>  ($maxy - $miny) + 10]);
+        imageJpeg($im2,'digitale-stopp-uhr-stoppuhr_copy.jpeg');
+
         $a = "<span style='font-size: 15px;'>$text</span>";
-        
-        $b = "<span style='flex: 1'><img src='Unbenannt.png' style='width: 100%;'></img></span>";
-        $c = "<span style='flex: 1'><img src='gradient.png' style='width: 100%;'></img></span>";
+        $b = "<span style='flex: 1'><img src='digitale-stopp-uhr-stoppuhr_copy.jpeg' style='width: 100%;'></img></span>";
+        $c = "<span style='flex: 1'><img src='digitale-stopp-uhr-stoppuhr.jpeg' style='width: 100%;'></img></span>";
 
         echo "<div style='display:flex;'>$a $b $c</div>";
+
+        dd($arr,['x' => $minx, 'y' => $miny,'maxx' => $maxx, 'maxy' => $maxy, 'width' => $maxx - $minx, 'height' =>  $maxy - $miny]);
+
+
+        imageDestroy($im2);
+        imageDestroy($image);
         exit;
 
         return $this->render('frontend/index.html.twig', [
             'controller_name' => 'FrontendController',
         ]);
     }
-
-
-    public function image_gradientrect($img,$x,$y,$x1,$y1,$start,$end) {
-        if($x > $x1 || $y > $y1) {
-           return false;
-        }
-        $s = array(
-           hexdec(substr($start,0,2)),
-           hexdec(substr($start,2,2)),
-           hexdec(substr($start,4,2))
-        );
-        $e = array(
-           hexdec(substr($end,0,2)),
-           hexdec(substr($end,2,2)),
-           hexdec(substr($end,4,2))
-        );
-        $steps = $y1 - $y;
-        for($i = 0; $i < $steps; $i++) {
-           $r = $s[0] - ((($s[0]-$e[0])/$steps)*$i);
-           $g = $s[1] - ((($s[1]-$e[1])/$steps)*$i);
-           $b = $s[2] - ((($s[2]-$e[2])/$steps)*$i);
-           $color = imagecolorallocate($img,$r,$g,$b);
-           imagefilledrectangle($img,$x,$y+$i,$x1,$y+$i+1,$color);
-        }
-        return true;
-     }
 
 }
